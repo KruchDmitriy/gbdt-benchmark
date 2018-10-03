1d3bb1b9421583783eb4087cfccbafb2ee6b00c0
KruchDmitriy <kruch.dmitriy@gmail.com>
2018-10-03 14:20:13 +0300

    added plots script

--- experiments.py
+++ experiments.py
@@ -1,8 +1,6 @@
-import argparse
-import json
+from sklearn.model_selection import train_test_split, ParameterGrid
 
-from learners import *
-from experiments import *
+import ml_dataset_loader.datasets as data_loader
 
 
 class Data:
@@ -66,46 +64,3 @@ DATASETS = {
     "msrank": Experiment(data_loader.get_msrank, "MSRank", "Regression", "RMSE"),
     "msrank-classification": Experiment(data_loader.get_msrank, "MSRank", "Multiclass", "Accuracy")
 }
-
-
-LEARNERS = {
-    "xgb": XGBoostLearner,
-    "lgb": LightGBMLearner,
-    "cat": CatBoostLearner
-}
-
-
-def _get_all_values_from_subset(items, subset):
-    filtered_keys = filter(lambda x: x in subset, items.keys())
-    return [items[key] for key in filtered_keys]
-
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--learners', nargs='+', choices=LEARNERS.keys(), required=True)
-    parser.add_argument('--datasets', nargs='+', choices=DATASETS.keys(), required=True)
-    parser.add_argument('--use-gpu', action='store_true')
-    parser.add_argument('--iterations', type=int, required=True)
-    parser.add_argument('--params-grid', default=None, help='path to json file, each key corresponds to learner parameter,\
-            e.g. max_depth, and list of values to run in experiment')
-    parser.add_argument('--eval-on-train', default=True, help='eval test metrics during training')
-    parser.add_argument('-o', '--out-dir', default='results')
-    args = parser.parse_args()
-
-    experiment_learners = _get_all_values_from_subset(LEARNERS, args.learners)
-    experiments = _get_all_values_from_subset(DATASETS, args.datasets)
-
-    params_grid = {
-    	"iterations": [args.iterations]
-    }
-
-    if args.params_grid:
-        with open(args.params_grid) as f:
-            grid = json.load(f)
-        params_grid.update(grid)
-
-
-    for experiment in experiments:
-    	print(experiment.name)
-    	experiment.run(args.use_gpu, experiment_learners, params_grid, args.eval_on_train, args.out_dir)
-
